1eecfe3945b1d92ea9ac675744167169c9701780
NazarKhirash <hirashnazar@ukt.net>
2022-03-07 22:46:50 +0200

    hw-1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Users from "./components/Users/Users";
+import Posts from "./components/Posts/Posts";
+import Comments from "./components/Comments/Comments";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='wrapper'>
+            <header className='header'>
+                <div className='header_box'>
+                    <Users/>
+                </div>
+                <div className='header_box'>
+                    <Posts/>
+                </div>
+            </header>
+            <div className='main'>
+                <Comments/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
